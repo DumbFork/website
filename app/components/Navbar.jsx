e2232f700042f2af08d0331568d54540c02a833e
DumbFork <168970999+DumbFork@users.noreply.github.com>
2024-05-14 18:26:15 +0545

    Blur filter

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
 return (
-   <nav className='fixed top-0 left-0 right-0 z-10 bg-[#021414] bg-opacity-90'>
+   <nav className='fixed top-0 left-0 right-0 z-10 bg- bg-[#021414] backdrop-blur-sm bg-opacity-50'>
         <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({imgUrl, title, description, gitUrl, previewUrl}) => {
         <div className='h-52 md:h-72 rounded-t-xl relative group'
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition:"center"}}
         >
-          <div className='overlay items-center justify-center absolute rounded-t-xl top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 trasition-all duration-500'>
+          <div className='overlay items-center justify-center absolute rounded-t-xl top-0 left-0 w-full h-full backdrop-blur-sm bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 trasition-all duration-500'>
             <Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
               <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
               </Link>
