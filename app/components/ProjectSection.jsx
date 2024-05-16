e2652e79e5b57c608c8944df20a8cec8206d5569
DumbFork <168970999+DumbFork@users.noreply.github.com>
2024-05-16 22:19:16 +0545

    notice

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -74,7 +74,7 @@ const ProjectsSection = () => {
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+        My Projects *Under development!!
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
